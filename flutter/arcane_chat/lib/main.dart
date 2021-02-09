1f84715c1320f8df120bed6fdbe49fbc25e299de
Daniel Mills <danielmillst@gmail.com>
2021-02-08 21:49:30 -0500

    Fesfd

--- flutter/arcane_chat/lib/main.dart
+++ flutter/arcane_chat/lib/main.dart
@@ -2,17 +2,17 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:web3dart/credentials.dart';
 import 'package:http/http.dart';
+import 'package:web3dart/credentials.dart';
 import 'package:web3dart/web3dart.dart';
 
 void main() {
   runApp(Arcane());
 }
 
-class Constant
-{
-  static final String endpoint = "https://goerli.infura.io/v3/a7c468d6a0914a10a94705cd83eddcf3";
+class Constant {
+  static final String endpoint =
+      "https://kovan.infura.io/v3/a7c468d6a0914a10a94705cd83eddcf3";
 }
 
 class Arcane extends StatefulWidget {
@@ -29,13 +29,16 @@ class _ArcaneState extends State<Arcane> {
   Credentials credentials;
   String exactAddress;
 
-  Future<bool> setup() async
-  {
+  Future<bool> setup() async {
+    wallet = Wallet.fromJson(
+        '{"version":3,"id":"2fbf0f5a-610c-45b2-8fd0-e6780acc0b2f","address":"908d4cba13452631da25b8b2b17896dd060815cb","crypto":{"ciphertext":"1a71a3cd5af3b35a9995d4787b231895062df0a41bb3cf0fae75930ac49d0573","cipherparams":{"iv":"533ef70a636f578099115431cc7d94f6"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"c40edd1a1e33e85ee1501e5670e971f6054c8b2fe81bc07fc390f25f6fbf81bf","n":8192,"r":8,"p":1},"mac":"811901eb3cead45f85c268d1cad64c47b741a129ffc191e9399407eaf3a38ee2"}}',
+        "(*Qz12311232)");
+    c = Client();
+    wc = Web3Client(Constant.endpoint, c);
     EthereumAddress a = (await wallet.privateKey.extractAddress());
     exactAddress = a.hex;
     credentials = await wc.credentialsFromPrivateKey(exactAddress);
-    
-    EtherAmount amt =await wc.getBalance(a);
+    EtherAmount amt = await wc.getBalance(a);
     print("Address: ${a.hex}");
     print("Value: ${amt.getValueInUnit(EtherUnit.ether)}");
     return true;
@@ -44,10 +47,6 @@ class _ArcaneState extends State<Arcane> {
   @override
   void initState() {
     super.initState();
-    wallet = Wallet.createNew(EthPrivateKey.createRandom(Random(1337)), "1337", Random(1337));
-    c  = Client();
-    wc = Web3Client(Constant.endpoint, c);
-    
   }
 
   @override
@@ -55,33 +54,34 @@ class _ArcaneState extends State<Arcane> {
     return MaterialApp(
       title: "Arcane",
       theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarBrightness: Brightness.dark,
-            statusBarIconBrightness: Brightness.light
-          )
+          appBarTheme: AppBarTheme(
+              systemOverlayStyle: SystemUiOverlayStyle(
+                  statusBarBrightness: Brightness.dark,
+                  statusBarIconBrightness: Brightness.light)),
+          primaryColor: Color(0xFF422fbd)),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Arcane"),
         ),
-        primaryColor: Color(0xFF422fbd)
+        body: FutureBuilder<bool>(
+          future: setup(),
+          builder: (context, snap) {
+            Size s = MediaQuery.of(context).size;
+            double w = min(s.width, s.height);
+            if (!snap.hasData) {
+              return Center(
+                child: Container(
+                  width: w / 3,
+                  height: w / 3,
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+
+            return Text(exactAddress);
+          },
+        ),
       ),
-      home: Scaffold(appBar: AppBar(
-        title: Text("Arcane"),
-      ), body: FutureBuilder<bool>(
-        future: setup(),
-        builder: (context, snap) {
-    Size s =  MediaQuery.of(context).size;
-    double w = min(s.width, s.height);
-          if(!snap.hasData)
-          { 
-            return Center(
-            child: Container(width: w/3, height: w/3, child: CircularProgressIndicator(
-
-            ),),
-          );
-          }
-
-          return Text(exactAddress);
-        },
-      ),),
     );
   }
 }
---
+++ flutter/arcane_chat/lib/walletgen.dart
@@ -0,0 +1,50 @@
+import 'dart:math';
+
+import 'package:web3dart/credentials.dart';
+
+class WalletGenerator {
+  Future<Wallet> generate(List<int> seedlings, String password) async {
+    int c = seedlings.length;
+    int ma = 9223372036854775807;
+    int mi = -9223372036854775808;
+    int g = 0;
+    int x = 0;
+    int z = 0;
+    Random rbuf = Random(-4483386 * c);
+    Random rbufx = Random(83386 + c);
+    z += rbuf.nextInt(ma) % 2 == 0 ? rbuf.nextInt(ma) : rbuf.nextInt(mi);
+
+    for (int i = 0; i < c; i++) {
+      Random rx = Random((c * i) + c - 495);
+      Random rx2 = Random((c * (i * 2)) - c + 415);
+      z += rbuf.nextInt(ma) % 2 == 0 ? rx.nextInt(ma) : rx.nextInt(mi);
+      z += rbufx.nextInt(ma) % 2 == 0 ? rx2.nextInt(ma) : rx2.nextInt(mi);
+      for (int j = 0; j < i + 1; j++) {
+        g = rx.nextInt(7) + 32 - rx2.nextInt(18);
+        z += rbuf.nextInt(ma) % 2 == 0 ? rx.nextInt(ma) : rx.nextInt(mi);
+        z += rbufx.nextInt(ma) % 2 == 0 ? rx2.nextInt(ma) : rx2.nextInt(mi);
+
+        for (int k = 0; k < g; k++) {
+          rx.nextDouble();
+          rx2.nextBool();
+          x += rx.nextInt(24 * k) + 1 + rx2.nextInt(k);
+          z += rbuf.nextInt(ma) % 2 == 0 ? rx.nextInt(ma) : rx.nextInt(mi);
+          z += rbufx.nextInt(ma) % 2 == 0 ? rx2.nextInt(ma) : rx2.nextInt(mi);
+        }
+
+        x += rx.nextInt(x + g - c - rx2.nextInt(mi));
+        x += rx2.nextInt(x - g + c + rx.nextInt(ma));
+        rx = Random(x + c - g + 344956 - z);
+        rx2 = Random(z * c + g - 3597 + z);
+        z += rbuf.nextInt(ma) % 2 == 0 ? rx.nextInt(ma) : rx.nextInt(mi);
+        z += rbufx.nextInt(ma) % 2 == 0 ? rx2.nextInt(ma) : rx2.nextInt(mi);
+      }
+
+      rbuf = Random(z + x + c + g + (ma - mi));
+      rbufx = Random(x * z - c - g + (ma - mi) * 235);
+    }
+
+    return new Wallet.createNew(
+        EthPrivateKey.createRandom(rbuf), password, rbufx);
+  }
+}
