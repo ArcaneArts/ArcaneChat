a681122e8b981930dd216b1db892b01f4a3eafb0
Daniel Mills <danielmillst@gmail.com>
2021-02-09 07:28:45 -0500

    Wallet manager id fixes

--- flutter/arcane_chat/lib/wallet_manager.dart
+++ flutter/arcane_chat/lib/wallet_manager.dart
@@ -4,13 +4,22 @@ import 'package:arcane_chat/arcane_storage.dart';
 import 'package:arcane_chat/satchel.dart';
 import 'package:web3dart/credentials.dart';
 
+typedef Progressor(double p);
+
 class WalletManager {
   static List<Satchel> getSatchels() {
     List<Satchel> s = List<Satchel>();
 
     ArcaneStorage.getWallets().values.forEach((element) {
       try {
-        s.add(Satchel.fromJson(element));
+        Satchel ss = Satchel.fromJson(element);
+
+        if (ss.data == null) {
+          print("Null data for satchel $element. Ignoring.");
+          return;
+        }
+
+        s.add(ss);
       } catch (e) {}
     });
 
@@ -19,6 +28,7 @@ class WalletManager {
 
   static void setSatchel(Satchel s) {
     ArcaneStorage.getWallets().put(s.id, s.toJsonString());
+    print("Saved " + s.toJsonString());
   }
 
   static Satchel getSatchel(String id) {
@@ -27,8 +37,8 @@ class WalletManager {
 
   static Future<Wallet> generate(List<int> seedlings, String password) async {
     int c = seedlings.length;
-    int ma = 9223372036854775807;
-    int mi = -9223372036854775808;
+    int ma = 7;
+    int mi = 1;
     int g = 0;
     int x = 0;
     int z = 0;
@@ -41,32 +51,28 @@ class WalletManager {
       Random rx2 = Random((c * (i * 2)) - c + 415);
       z += rbuf.nextInt(ma) % 2 == 0 ? rx.nextInt(ma) : rx.nextInt(mi);
       z += rbufx.nextInt(ma) % 2 == 0 ? rx2.nextInt(ma) : rx2.nextInt(mi);
-      for (int j = 0; j < i + 1; j++) {
-        g = rx.nextInt(7) + 32 - rx2.nextInt(18);
-        z += rbuf.nextInt(ma) % 2 == 0 ? rx.nextInt(ma) : rx.nextInt(mi);
-        z += rbufx.nextInt(ma) % 2 == 0 ? rx2.nextInt(ma) : rx2.nextInt(mi);
-
-        for (int k = 0; k < g; k++) {
-          rx.nextDouble();
-          rx2.nextBool();
-          x += rx.nextInt(24 * k) + 1 + rx2.nextInt(k);
-          z += rbuf.nextInt(ma) % 2 == 0 ? rx.nextInt(ma) : rx.nextInt(mi);
-          z += rbufx.nextInt(ma) % 2 == 0 ? rx2.nextInt(ma) : rx2.nextInt(mi);
-        }
-
-        x += rx.nextInt(x + g - c - rx2.nextInt(mi));
-        x += rx2.nextInt(x - g + c + rx.nextInt(ma));
-        rx = Random(x + c - g + 344956 - z);
-        rx2 = Random(z * c + g - 3597 + z);
-        z += rbuf.nextInt(ma) % 2 == 0 ? rx.nextInt(ma) : rx.nextInt(mi);
-        z += rbufx.nextInt(ma) % 2 == 0 ? rx2.nextInt(ma) : rx2.nextInt(mi);
-      }
-
       rbuf = Random(z + x + c + g + (ma - mi));
       rbufx = Random(x * z - c - g + (ma - mi) * 235);
     }
 
     return new Wallet.createNew(
         EthPrivateKey.createRandom(rbuf), password, rbufx);
+  }
+
+  static String nextSatchelId() {
+    int m = 0;
+
+    while (true) {
+      m++;
+      String id = "satchel-" + ("s-$m".hashCode).toString();
+
+      if (!ArcaneStorage.getWallets().keys.contains(id)) {
+        return id;
+      }
+    }
+  }
+
+  static void deleteSatchel(Satchel satchel) {
+    ArcaneStorage.getWallets().delete(satchel.id);
   }
 }
