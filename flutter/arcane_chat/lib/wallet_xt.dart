3e60b186faa7d28bece66bcdda0decb789438e36
Daniel Mills <danielmillst@gmail.com>
2021-02-10 01:30:31 -0500

    Wallet extension

---
+++ flutter/arcane_chat/lib/wallet_xt.dart
@@ -0,0 +1,25 @@
+import 'package:arcane_chat/arcane_connect.dart';
+import 'package:arcane_chat/arcaneamount.dart';
+import 'package:arcane_chat/constant.dart';
+import 'package:web3dart/credentials.dart';
+import 'package:web3dart/web3dart.dart';
+
+extension WalletXT on Wallet {
+  Future<EthereumAddress> getAddress() => this.privateKey.extractAddress();
+
+  Future<EtherAmount> getBalance() =>
+      getAddress().then((value) => ArcaneConnect.connect().getBalance(value));
+
+  Future<double> getUSD() => getEther()
+      .then((value) => ArcaneConnect.getUSDPrice().then((usd) => usd * value));
+
+  Future<double> getMana() =>
+      getEther().then((value) => value * Constant.MANA_PER_ETH);
+
+  Future<double> getEther() => getBalance()
+      .then((value) => value.getValueInUnit(EtherUnit.ether).toDouble());
+
+  Future<ArcaneAmount> getArcaneBalance() =>
+      getEther().then((value) => ArcaneConnect.getUSDPrice()
+          .then((usd) => ArcaneAmount(usdPrice: usd, value: value)));
+}
