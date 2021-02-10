bb49e2cb29186382031e934592aace1f507e3459
Daniel Mills <danielmillst@gmail.com>
2021-02-10 02:48:42 -0500

    Constant support chain ids

--- flutter/arcane_chat/lib/constant.dart
+++ flutter/arcane_chat/lib/constant.dart
@@ -1,9 +1,13 @@
 class Constant {
   static final int SEED_BITS = 256;
-  static final double MANA_PER_ETH = 100000;
+  static final double MANA_PER_ETH = (100 * 1000).toDouble();
+  static final double GAS_LIMIT_SEND = (60 * 1000).toDouble();
   static final String CRYPTO_COMPARE_API_KEY =
       "1d37c00d2f956ade5ec565c419745e7a08f559a7539c0b666c07b2e7eced81f7";
+  static final int CHAIN_ID_KOVAN = 42;
+  static final int CHAIN_ID_MAIN = 1;
   static final String INFURA_KOVAN_API =
       "https://kovan.infura.io/v3/a7c468d6a0914a10a94705cd83eddcf3";
   static final String INFURA_API = INFURA_KOVAN_API;
+  static final int CHAIN_ID = CHAIN_ID_KOVAN;
 }
