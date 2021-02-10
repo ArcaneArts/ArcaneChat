c4a825f5148b5bfb25d97382fc06b24f822f6dae
Daniel Mills <danielmillst@gmail.com>
2021-02-10 01:30:12 -0500

    Arcane view screen

--- flutter/arcane_chat/lib/arcane_view.dart
+++ flutter/arcane_chat/lib/arcane_view.dart
@@ -1,4 +1,5 @@
 import 'package:arcane_chat/arcane_account_settings.dart';
+import 'package:arcane_chat/arcane_wallet_view.dart';
 import 'package:arcane_chat/satchel.dart';
 import 'package:flutter/material.dart';
 import 'package:web3dart/web3dart.dart';
@@ -17,9 +18,32 @@ class _ArcaneViewState extends State<ArcaneView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Text("Derp"),
       appBar: AppBar(
         title: Text("Arcane"),
         actions: [
+          Material(
+            child: Hero(
+              child: Material(
+                child: IconButton(
+                  icon: Icon(
+                    Icons.account_balance_wallet_rounded,
+                  ),
+                  tooltip: "Wallet",
+                  onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ArcaneWalletView(
+                                satchel: widget.satchel,
+                                wallet: widget.wallet,
+                              ))),
+                ),
+                color: Colors.transparent,
+              ),
+              tag: "card",
+            ),
+            color: Colors.transparent,
+          ),
           IconButton(
               icon: Icon(Icons.settings),
               onPressed: () => Navigator.push(
