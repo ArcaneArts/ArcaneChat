9286a11495af5153bc1d0a28dfa907f3b52233bc
Daniel Mills <danielmillst@gmail.com>
2021-02-10 01:30:20 -0500

    Arcane wallet views

---
+++ flutter/arcane_chat/lib/arcane_wallet_view.dart
@@ -0,0 +1,142 @@
+import 'package:arcane_chat/arcane_receive.dart';
+import 'package:arcane_chat/arcane_send.dart';
+import 'package:arcane_chat/arcaneamount.dart';
+import 'package:arcane_chat/satchel.dart';
+import 'package:arcane_chat/wallet_xt.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:web3dart/credentials.dart';
+import 'package:web3dart/web3dart.dart';
+
+class ArcaneWalletView extends StatefulWidget {
+  final Satchel satchel;
+  final Wallet wallet;
+  ArcaneWalletView({this.satchel, this.wallet});
+  @override
+  _ArcaneWalletViewState createState() => _ArcaneWalletViewState();
+}
+
+class _ArcaneWalletViewState extends State<ArcaneWalletView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Padding(
+          child: Material(
+            child: Hero(
+              child: Material(
+                child: Card(
+                  child: Container(
+                    width: 350,
+                    child: Padding(
+                      padding: EdgeInsets.all(14),
+                      child: Stack(
+                        children: [
+                          IconButton(
+                              icon: Icon(Icons.arrow_back),
+                              onPressed: () => Navigator.pop(context)),
+                          FutureBuilder<ArcaneAmount>(
+                            future: widget.wallet.getArcaneBalance(),
+                            builder: (context, snap) {
+                              if (!snap.hasData) {
+                                return Container(
+                                  width: 100,
+                                  height: 100,
+                                  child: CircularProgressIndicator(),
+                                );
+                              }
+                              NumberFormat nf = NumberFormat();
+
+                              return Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Flexible(
+                                      child: Icon(
+                                    Icons.account_balance_wallet,
+                                    size: 82,
+                                  )),
+                                  Flexible(
+                                      child: Text(
+                                    "${nf.format(snap.data.getMana())} Mana",
+                                    style: TextStyle(fontSize: 36),
+                                  )),
+                                  Flexible(
+                                      child: Text(
+                                    "${nf.format(snap.data.getEther())} ETH",
+                                    style: TextStyle(
+                                        fontSize: 21,
+                                        color: Theme.of(context)
+                                            .textTheme
+                                            .subtitle1
+                                            .color),
+                                  )),
+                                  Flexible(
+                                      child: Text(
+                                    "${nf.format(snap.data.getUsd())} USD",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        color: Theme.of(context)
+                                            .textTheme
+                                            .subtitle1
+                                            .color),
+                                  )),
+                                  Flexible(
+                                      child: Padding(
+                                    child: Container(
+                                      height: 0.3,
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .subtitle1
+                                          .color,
+                                    ),
+                                    padding: EdgeInsets.only(
+                                        left: 14,
+                                        right: 14,
+                                        top: 14,
+                                        bottom: 4),
+                                  )),
+                                  Flexible(
+                                      child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(
+                                          icon: Icon(Icons.save_alt),
+                                          onPressed: () => Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      ArcaneReceive(
+                                                        satchel: widget.satchel,
+                                                        wallet: widget.wallet,
+                                                      )))),
+                                      IconButton(
+                                          icon: Icon(Icons.send),
+                                          onPressed: () => Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      ArcaneSend()))),
+                                    ],
+                                  ))
+                                ],
+                              );
+                            },
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                color: Colors.transparent,
+              ),
+              tag: "card",
+            ),
+            color: Colors.transparent,
+          ),
+          padding: EdgeInsets.all(14),
+        ),
+      ),
+    );
+  }
+}
