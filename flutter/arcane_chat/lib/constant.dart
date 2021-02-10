12b75363143e65d9368c946415ea76f7755a0ffc
Daniel Mills <danielmillst@gmail.com>
2021-02-10 01:30:27 -0500

    Constant additions

--- flutter/arcane_chat/lib/constant.dart
+++ flutter/arcane_chat/lib/constant.dart
@@ -1,3 +1,9 @@
 class Constant {
   static final int SEED_BITS = 256;
+  static final double MANA_PER_ETH = 100000;
+  static final String CRYPTO_COMPARE_API_KEY =
+      "1d37c00d2f956ade5ec565c419745e7a08f559a7539c0b666c07b2e7eced81f7";
+  static final String INFURA_KOVAN_API =
+      "https://kovan.infura.io/v3/a7c468d6a0914a10a94705cd83eddcf3";
+  static final String INFURA_API = INFURA_KOVAN_API;
 }
