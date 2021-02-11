7eba687bac1566de8213c157719dc2e626816c4d
Daniel Mills <danielmillst@gmail.com>
2021-02-10 20:12:58 -0500

    Theme tweaks

--- flutter/arcane_chat/lib/main.dart
+++ flutter/arcane_chat/lib/main.dart
@@ -31,7 +31,24 @@ class _ArcaneState extends State<Arcane> {
               systemOverlayStyle: SystemUiOverlayStyle(
                   statusBarBrightness: Brightness.dark,
                   statusBarIconBrightness: Brightness.light)),
-          primaryColor: Color(0xFF422fbd)),
+          primaryColor: Color(0xFF422fbd),
+          splashColor: Color(0x86422fbd),
+          textSelectionColor: Color(0x11422fbd),
+          textButtonTheme: TextButtonThemeData(
+              style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all(Color(0xFF422fbd)))),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Color(0xFF422fbd)))),
+          textSelectionTheme: TextSelectionThemeData(
+              selectionColor: Color(0x88422fbd),
+              cursorColor: Color(0xFF422fbd)),
+          cursorColor: Color(0xFF422fbd),
+          buttonTheme: ButtonThemeData(buttonColor: Color(0xFF422fbd)),
+          highlightColor: Color(0x86422fbd),
+          accentColor: Color(0xFF422fbd)),
       home: ArcaneAccountSelector(),
     );
   }
