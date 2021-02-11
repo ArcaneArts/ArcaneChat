913a24c901f9e9341d38f0c76b5103b4e12a38f8
Daniel Mills <danielmillst@gmail.com>
2021-02-10 20:10:49 -0500

    Connect Mana fee calculation

--- flutter/arcane_chat/lib/arcane_connect.dart
+++ flutter/arcane_chat/lib/arcane_connect.dart
@@ -11,6 +11,32 @@ class ArcaneConnect {
 
   static ArcaneContract getContract() => ArcaneContract.connect();
 
+  static Future<int> getManaFee() async {
+    double gasPrice = (await ArcaneConnect.connect().getGasPrice())
+        .getValueInUnit(EtherUnit.gwei)
+        .toDouble();
+    EtherAmount fee = EtherAmount.fromUnitAndValue(
+        EtherUnit.gwei, (gasPrice * Constant.GAS_LIMIT_SEND).toInt());
+    return (Constant.MANA_PER_ETH *
+            fee.getValueInUnit(EtherUnit.ether).toDouble())
+        .toInt();
+  }
+
+  static Future<bool> waitForTx(Future<String> f) {
+    return f.onError((error, stackTrace) => null).then((value) {
+      if (value == null) {
+        print("TX NULL?");
+        return false;
+      } else {
+        return waitForTxHash(value);
+      }
+    });
+  }
+
+  static void reset() {
+    _client = null;
+  }
+
   static Web3Client connect() {
     if (_client == null) {
       _client = new Web3Client(Constant.INFURA_API, Client());
@@ -31,4 +57,22 @@ class ArcaneConnect {
                     .toString()) ??
             0);
       });
+
+  static Future<bool> waitForTxHash(String value) async {
+    TransactionInformation info = await connect()
+        .getTransactionByHash(value)
+        .onError((error, stackTrace) => null);
+
+    if (info == null) {
+      print("Couldnt find Tx $value. Waiting 10s");
+      return Future.delayed(Duration(seconds: 10), () => waitForTxHash(value));
+    }
+
+    if (info.blockNumber.isPending) {
+      print("Couldnt find real block for Tx $value. Waiting 6s");
+      return Future.delayed(Duration(seconds: 6), () => waitForTxHash(value));
+    }
+
+    return true;
+  }
 }
