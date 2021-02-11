560ad488613c0e2ae5dea9dfdf90de83433d2a7e
Daniel Mills <danielmillst@gmail.com>
2021-02-10 20:12:47 -0500

    Gss limit fixes

--- flutter/arcane_chat/lib/constant.dart
+++ flutter/arcane_chat/lib/constant.dart
@@ -1,7 +1,8 @@
 class Constant {
+  static final int BLOCK_WALK_BATCH_SIZE = 10000; // 4 blocks/m 240 blocks/h
   static final int SEED_BITS = 256;
   static final double MANA_PER_ETH = (100 * 1000).toDouble();
-  static final double GAS_LIMIT_SEND = (60 * 1000).toDouble();
+  static final double GAS_LIMIT_SEND = (300 * 1000).toDouble();
   static final String CRYPTO_COMPARE_API_KEY =
       "1d37c00d2f956ade5ec565c419745e7a08f559a7539c0b666c07b2e7eced81f7";
   static final int CHAIN_ID_KOVAN = 42;
